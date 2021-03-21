829a0148f978ef147e5012f1cff85caddd796774
Bharat <bharathmudragada123@gmail.com>
2021-03-21 18:45:57 +0530

    [Coupons] Updated Api's to store user, entity and product ids using json dumps instead of serparate tables to improve timings Added Terms & Conditions for coupon

--- ChillBro/Coupons/urls.py
+++ ChillBro/Coupons/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import Discount, UseCoupon, CouponList, CouponDetail, CouponHistoryList, AvailableCoupons
 
 urlpatterns = [
@@ -6,6 +6,6 @@ urlpatterns = [
     path('use_coupon/', UseCoupon.as_view()),
     path('', CouponList.as_view()),
     path('<int:pk>/', CouponDetail.as_view()),
-    path('history/', CouponHistoryList.as_view()),
+    re_path('^history/(?P<slug>[-\w]+)/$', CouponHistoryList.as_view()),
     path('available/', AvailableCoupons.as_view()),
 ]
