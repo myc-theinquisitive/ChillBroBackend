829140cbc71ea76314a800c09cbcadb4ee70575e
Bharat <bharathmudragada123@gmail.com>
2021-04-25 17:26:12 +0530

    Modified issues code and added permission classes

--- ChillBro/Issues/serializers.py
+++ ChillBro/Issues/serializers.py
@@ -2,24 +2,53 @@ from rest_framework import serializers
 from .models import *
 
 
+class IssueImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IssueImage
+        fields = '__all__'
+
+    @staticmethod
+    def bulk_create(issue_images):
+        all_images = []
+        for image in issue_images:
+            each_image = IssueImage(
+                issue=image['issue'],
+                image=image['image']
+            )
+            all_images.append(each_image)
+        return IssueImage.objects.bulk_create(all_images)
+
+
 class IssueSerializer(serializers.ModelSerializer):
+    images = serializers.ListField(
+        child=serializers.FileField(), write_only=True
+    )
+
     class Meta:
         model = Issue
         fields = "__all__"
 
+    def create(self, validated_data):
+        return Issue.objects.create(
+            created_by_id=validated_data["created_by"], title=validated_data["title"],
+            description=validated_data["description"], entity_id=validated_data["entity_id"],
+            order_id=validated_data["order_id"], product_id=validated_data["product_id"]
+        )
 
-class EditIssueSerializer(serializers.ModelSerializer):
+
+class PickCloseIssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields = ["id","status","current_employeeId","final_resolution"]
-    final_resolution = serializers.CharField(max_length=200, required=True)
-    current_employeeId = serializers.CharField(max_length=30, required=True)
+        fields = ['final_resolution', 'status', 'current_employee_id']
+
+    final_resolution = serializers.CharField(required=True)
+    current_employee_id = serializers.CharField(max_length=36, required=True)
 
 
 class CloseUserIssueSerializer(serializers.ModelSerializer):
     class Meta:
         model = Issue
-        fields = ['final_resolution', 'status', 'user_id', 'current_employeeId']
+        fields = ['final_resolution', 'status', 'current_employee_id']
 
 
 class TransferSerializer(serializers.ModelSerializer):
--- ChillBro/Issues/views.py
+++ ChillBro/Issues/views.py
@@ -1,101 +1,139 @@
-# Create your views here.
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import transaction
 from rest_framework.generics import ListCreateAPIView
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
-from .constants import Status, DEFAULT_FINAL_RESOLUTION
+from .constants import Status, DEFAULT_FINAL_RESOLUTION, USER_CLOSED_RESOLUTION
+from .wrappers import is_order_id_valid, is_product_id_valid
+from ChillBro.permissions import IsSuperAdminOrMYCEmployee, IsOwner
 
 
 class IssueList(ListCreateAPIView):
     serializer_class = IssueSerializer
-
     permission_classes = (IsAuthenticated,)
 
-    def post(self, request):
-        request.data['user_id'] = request.user.id
+    def post(self, request, *args, **kwargs):
+        request.data._mutable = True
+        request.data['created_by'] = request.user.id
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Issue inserted successfully", "issue id": serializer.data['id']},
-                            status=status.HTTP_200_OK)
-        else:
+
+        if not is_product_id_valid(request.data['product_id']):
+            return Response({"message": "Issue not inserted", "error": "Invalid Product Id"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if not is_order_id_valid(request.data['order_id']):
+            return Response({"message": "Issue not inserted", "error": "Invalid Order Id"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if not serializer.is_valid():
             return Response({"message": "Issue not inserted", "error": serializer.errors},
                             status=status.HTTP_400_BAD_REQUEST)
 
-    def get(self, request):
-        issues = Issue.objects.all()
-        serializer = self.serializer_class(issues, many=True)
-        return Response(serializer.data)
+        images = request.data.pop("images", [])
+        issue = serializer.create(request.data.dict())
+
+        issue_image_dicts = []
+        for image in images:
+            issue_image_dict = {
+                "issue": issue,
+                "image": image
+            }
+            issue_image_dicts.append(issue_image_dict)
+        IssueImageSerializer.bulk_create(issue_image_dicts)
+
+        return Response({"message": "Issued inserted successfully", "issue_id": issue.id},
+                        status=status.HTTP_200_OK)
 
 
-class IssueDetail(generics.RetrieveUpdateDestroyAPIView):
+class IssueDetail(generics.RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
 
-    def put(self, request,pk):
-        try:
-            issue = Issue.objects.get(id=pk)
-        except:
-            return Response({"error": "Issue doesn't exist"},status=status.HTTP_404_NOT_FOUND)
-        request.data['user_id'] = request.user.id
-        serializer = self.serializer_class(issue, data=request.data)
-        if serializer.is_valid():
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, args, kwargs)
+        issue = self.get_object()
+        images = IssueImage.objects.filter(issue=issue).values_list("image", flat=True)
+        response.data["images"] = images
+        return response
+
+
+class PickCloseIssue(APIView):
+    serializer_class = PickCloseIssueSerializer
+    permission_classes = (IsAuthenticated & IsSuperAdminOrMYCEmployee,)
+
+    def put(self, request, issue_id, status_flag):
+        with transaction.atomic():
+            try:
+                issue = Issue.objects.select_for_update().get(pk=issue_id)
+            except ObjectDoesNotExist:
+                return Response({"message": "Issue can't be picked/closed", "error": "Invalid Issue"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            if issue.status == Status.DONE.value:
+                return Response({"message": "Issue can't be picked/closed", "error": "Issue already closed"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            request.data['status'] = status_flag
+            request.data['current_employee_id'] = request.user.id
+            if status_flag == Status.IN_PROGRESS.value:  # for issue to pick, final resolution need not be provided.
+                if issue.current_employee_id is not None:
+                    return Response({"message": "Issue can't be picked", "error": "Issue already picked"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                request.data['final_resolution'] = DEFAULT_FINAL_RESOLUTION
+            else:
+                # for closing an issue
+                if issue.current_employee_id != str(request.user.id):
+                    return Response({"message": "Issue can't be closed", "error": "You can't close this Issue"},
+                                    status=status.HTTP_400_BAD_REQUEST)
+
+            serializer = self.serializer_class(issue, data=request.data)
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class EditIssue(APIView):
-    serializer_class = EditIssueSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def put(self, request, status_flag):
-        try:
-            issue = Issue.objects.get(pk=request.data['issue_id'])
-        except:
-            return Response({"error": "Issue doesn't exist"},status=status.HTTP_404_NOT_FOUND)
-        request.data['status'] = status_flag
-        if status_flag == Status.IN_PROGRESS.value:  # for issue to pick, final resolution need not be provided.
-            request.data['final_resolution'] = DEFAULT_FINAL_RESOLUTION
-        serializer = self.serializer_class(issue, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class CloseIssueUser(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated & IsOwner,)
     serializer_class = CloseUserIssueSerializer
 
-    def put(self, request):
-        try:
-            issue = Issue.objects.get(pk=request.data['issue_id'])
-        except:
-            return Response({"error": "Issue doesn't exist"},status=status.HTTP_404_NOT_FOUND)
-        request.data['user_id'] = request.user.id
-        request.data['status'] = Status.DONE.value
-        request.data['final_resolution'] = "Issue closed by user"
-        request.data['current_employeeId'] = None
-        serializer = self.serializer_class(issue, data=request.data)
-        if serializer.is_valid():
+    def put(self, request, issue_id):
+        with transaction.atomic():
+            try:
+                issue = Issue.objects.select_for_update().get(pk=issue_id)
+            except ObjectDoesNotExist:
+                return Response({"message": "Issue can't be closed", "error": "Invalid Issue"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            self.check_object_permissions(request, issue)
+
+            if issue.status == Status.DONE.value:
+                return Response({"message": "Issue can't be closed", "error": "Issue already closed"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            request.data['status'] = Status.DONE.value
+            request.data['final_resolution'] = USER_CLOSED_RESOLUTION
+            request.data['current_employee_id'] = None
+            serializer = self.serializer_class(issue, data=request.data)
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class DepartmentStatusIssues(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
-    serializer_class=IssueSerializer
+    serializer_class = IssueSerializer
 
     def get_queryset(self):
-        queryset = Issue.objects.filter(current_department=self.kwargs['current_department'],
-                                           status=self.kwargs['status']).order_by('created_at')
+        queryset = Issue.objects.filter(
+            current_department=self.kwargs['current_department'], status=self.kwargs['status']).order_by('created_at')
         return queryset
 
 
@@ -103,24 +141,41 @@ class TransferIssue(APIView):
     permission_classes = (IsAuthenticated,)
     serializer_class = TransferSerializer
 
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            issue_id = request.data['issue_id']
-            issue = Issue.objects.get(id=issue_id)
-            issue.status = Status.TODO.value
-            issue.current_department = request.data['transferred_to']
-            issue.current_employeeId = None
-            issue.save()
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
+
+        try:
+            issue = Issue.objects.get(pk=request.data['issue_id'])
+        except ObjectDoesNotExist:
+            return Response({"message": "Issue can't be transferred", "error": "Invalid Issue"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if issue.status == Status.DONE.value:
+            return Response({"message": "Issue can't be transferred", "error": "Issue already closed"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        if issue.current_employee_id != str(request.user.id):
+            return Response({"message": "Issue can't be transferred", "error": "You can't transfer this Issue"},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        request.data["transferred_by"] = request.user.id
+        request.data["issue"] = issue.id
+
+        if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+        issue.status = Status.TODO.value
+        issue.current_department = request.data['transferred_to']
+        issue.current_employee_id = None
+        issue.save()
+
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TransferHistory(generics.ListAPIView):
     permission_classes = (IsAuthenticated,)
-    serializer_class=TransferSerializer
+    serializer_class = TransferSerializer
 
     def get_queryset(self):
         queryset = Transfer.objects.filter(issue_id=self.kwargs['issue_id']).order_by('created_at')
