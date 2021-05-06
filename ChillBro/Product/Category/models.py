a8f9e3c82dae7eef1a30014a3c4a15279124da88
Bharat <bharathmudragada123@gmail.com>
2021-05-06 19:27:25 +0530

    made changes

--- ChillBro/Product/Category/models.py
+++ ChillBro/Product/Category/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-from .helpers import uploadImageToCategoryIcons, iconUrlImage
+from .helpers import upload_image_to_category, update_image_to_category_icon
 
 
 class Category(models.Model):
     name = models.CharField(max_length=20, unique=True)
     description = models.TextField()
     parent_category = models.ForeignKey('self', on_delete=models.CASCADE,
-                                        blank=True, null=True, verbose_name="Parent Category")
-    icon_url = models.ImageField(upload_to=iconUrlImage)
+                                        null=True, verbose_name="Parent Category")
+    icon_url = models.ImageField(upload_to=update_image_to_category_icon)
 
     def __str__(self):
         return "Category - Nº{0}".format(self.name)
@@ -15,7 +15,7 @@ class Category(models.Model):
 
 class CategoryImage(models.Model):
     category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to=uploadImageToCategoryIcons)
+    image = models.ImageField(upload_to=upload_image_to_category)
     order = models.IntegerField()
 
     class Meta:
